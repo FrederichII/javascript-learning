82b149c4ea9e9d48d24d035f64b64bb677309a1a
Zhang <12012724@mail.sustech.edu.cn>
2023-07-18 23:49:05 +0800

    async, promise, await, ES6 modules, DOM intro, element selectors

---
+++ ES6 modules/DOM_quick_intro.js
@@ -0,0 +1,9 @@
+// console.log(document)
+console.dir(document);
+console.log(document.URL);
+
+document.title="Title goes here!";
+//document.location="http://www.google.com";
+
+document.body.style.backgroundColor="skyblue";
+document.getElementById("mydiv").innerHTML="hello";
